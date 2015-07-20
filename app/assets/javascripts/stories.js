4f81e3c4dd79f818c258dbd8d9d87d2feceb5d44
ErinUC <erinuc@gmail.com>
2015-07-20 18:34:22 -0400

    JS toggle functionality now hides all questions except for present one

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -1,6 +1,90 @@
 $(function(){
     $("#question02").hide();
-    $("#preview").on("click", function(){
+    $("#preview1").on("click", function(){
         $("#question01, #question02").toggle();
+    });
+});
+
+$(function(){
+    $("#question03").hide();
+    $("#preview2").on("click", function(){
+        $("#question02, #question03").toggle();
+    });
+});
+
+$(function(){
+    $("#question04").hide();
+    $("#preview3").on("click", function(){
+        $("#question03, #question04").toggle();
+    });
+});
+
+$(function(){
+    $("#question05").hide();
+    $("#preview4").on("click", function(){
+        $("#question04, #question05").toggle();
+    });
+});
+
+$(function(){
+    $("#question06").hide();
+    $("#preview5").on("click", function(){
+        $("#question05, #question06").toggle();
+    });
+});
+
+$(function(){
+    $("#question07").hide();
+    $("#preview6").on("click", function(){
+        $("#question06, #question07").toggle();
+    });
+});
+
+$(function(){
+    $("#question08").hide();
+    $("#preview7").on("click", function(){
+        $("#question07, #question08").toggle();
+    });
+});
+
+$(function(){
+    $("#question09").hide();
+    $("#preview8").on("click", function(){
+        $("#question08, #question09").toggle();
+    });
+});
+
+$(function(){
+    $("#question10").hide();
+    $("#preview9").on("click", function(){
+        $("#question09, #question10").toggle();
+    });
+});
+
+$(function(){
+    $("#question11").hide();
+    $("#preview10").on("click", function(){
+        $("#question10, #question11").toggle();
+    });
+});
+
+$(function(){
+    $("#question12").hide();
+    $("#preview11").on("click", function(){
+        $("#question11, #question12").toggle();
+    });
+});
+
+$(function(){
+    $("#question13").hide();
+    $("#preview12").on("click", function(){
+        $("#question12, #question13").toggle();
+    });
+});
+
+$(function(){
+    $("#question14").hide();
+    $("#preview13").on("click", function(){
+        $("#question13, #question14").toggle();
     });
 });
